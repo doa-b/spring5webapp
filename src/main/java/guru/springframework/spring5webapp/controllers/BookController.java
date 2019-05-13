f5ffdc768e5c1b6fd2be7878c9805fb7975e9903
doa-b <doa.bonifacio@gmail.com>
2019-05-13 12:14:20 +0200

    Finished adding simple MVC controller en View for Books & Authors

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,32 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Doa on 13-5-2019.
+ */
+
+//Registers the class a a Spring Bean AND as a controller in Spring MVC
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping ("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+
+    }
+
+}
